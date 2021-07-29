766294f1cd464b5a4b950973aa59479ee1f9d254
Lolbird123 <65594081+Lolbird123@users.noreply.github.com>
2021-07-28 22:52:27 -0500

    Create setup-passwd.lua

---
+++ passwd/setup-passwd.lua
@@ -0,0 +1,9 @@
+local fs = require("filesystem");
+if fs.exists("/etc/password") == false then
+  local file = io.open("/etc/password", "w");
+  file.stream:write("password");
+  file.stream:close();
+end
+local file2 = io.open("/etc/profile.lua", "a");
+file2.stream:write("\ndofile(\"/bin/lock.lua\")");
+file2.stream:close();
