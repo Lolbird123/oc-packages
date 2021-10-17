f4e601cd3ce6e7ee9a6fc6e2fbb6da5ac5ef1b12
Lolbird123 <65594081+Lolbird123@users.noreply.github.com>
2021-10-16 21:52:58 -0500

    add check action

--- lights/lights.lua
+++ lights/lights.lua
@@ -10,6 +10,8 @@ if args[1] == "on" then
   rs.setBundledOutput(sides.top, {[colors.white] = 255});
 elseif args[1] == "off" then
   rs.setBundledOutput(sides.top, {[colors.white] = 0});
+elseif args[1] == "check" then
+  print(rs.getBundledOutput(sides.top, colors.white));
 else
   print("Unknown action.");
 end
