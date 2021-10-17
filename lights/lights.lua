d6a5c8ce70728113b981af9322d7f16b031a707f
Lolbird123 <65594081+Lolbird123@users.noreply.github.com>
2021-10-16 21:26:57 -0500

    Create lights.lua
    (for a project im working on)

---
+++ lights/lights.lua
@@ -0,0 +1,13 @@
+local colors = require("colors");
+local component = require("component");
+local sides = require("sides");
+local shell = require("shell");
+
+local args = shell.parse(...);
+local rs = component.redstone;
+
+if args[1] == "on" then
+  rs.setBundledOutput(sides.top, {[colors.white] = 255});
+elseif args[1] == "off" then
+  rs.setBundledOutput(sides.top, {[colors.white] = 0});
+else
