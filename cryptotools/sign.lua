bc2ad3d25667f2e4207c0ca10f8aa0b4ba8fbfde
Lolbird <65594081+Lolbird123@users.noreply.github.com>
2023-09-04 17:03:45 -0500

    qwerty

--- cryptotools/sign.lua
+++ cryptotools/sign.lua
@@ -12,23 +12,20 @@ local data = component.data
 if opts["help"] or opts["h"] or not args[1] then
   print("sign [options] <file>")
   print("-h --help : show this")
-  print("-p --path <path> : set a custom path to load keys from instead of /home")
-  print("-n --name <name> : set a custom name to save keys as instead of default")
+  print("-p --path <path> : set a custom path to load private key from instead of /home/default.priv")
   print("-s --sig <name> : specify a custom signature suffix instead of .sig")
   return
 end
 
-local path = opts["path"] or opts["p"] or "/home"
-local name = opts["name"] or opts["n"] or "default"
+local key_path = opts["path"] or opts["p"] or "/home/default.priv"
 local sigsuf = opts["sig"] or opts["s"] or ".sig"
 local file = shell.resolve(args[1])
-local key_file = path.."/"..name..".priv"
 
 if not filesystem.exists(file) then
   print("file not found")
   return
 end
-if not filesystem.exists(key_file) then
+if not filesystem.exists(key_path) then
   print("private key not found")
   return
 end
@@ -36,7 +33,7 @@ local f
 f = filesystem.open(file, "r")
 local content = f:read(math.huge)
 f:close()
-f = filesystem.open(key_file, "r")
+f = filesystem.open(key_path, "r")
 local key = f:read(math.huge)
 f:close()
 key = data.deserializeKey(key, "ec-private")
